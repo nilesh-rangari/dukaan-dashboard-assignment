27fd0f5d8f0f8ac3e7ab0afa69db44ae9875a9b3
Nilesh <nvrangari@gmail.com>
2024-01-09 12:46:58 +0530

    Navbar Coded

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar"
+
+
+function Dashboard() {
+    return(
+        <div className='flex h-screen bg-emerald-400 font-["Inter"]'>
+            <Navbar/>
+
+        </div>
+    )
+}
+
+export default Dashboard
